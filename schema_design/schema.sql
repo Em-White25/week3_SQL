aa577453ea737a39b25723285b5c3d8c76197010
White <emmanuel.white@amalitechtraining.org>
2025-04-13 20:08:05 +0000

    modified script

--- schema_design/schema.sql
+++ schema_design/schema.sql
@@ -28,21 +28,23 @@ CREATE TABLE orders (
     status VARCHAR(20)
 );
 
--- Order Items table
-CREATE TABLE order_items (
+-- Order Details table
+CREATE TABLE order_details (
     details_id SERIAL PRIMARY KEY,
     order_id INT REFERENCES orders(order_id),
     product_id INT REFERENCES products(product_id),
     quantity INT,
-    unit_price NUMERIC(10, 2),
-    total_amount NUMERIC(10, 2)
+
 );
 
--- Inventory table
-CREATE TABLE inventory (
+-- Inventory Logs table
+CREATE TABLE inventory_logs (
     log_id SERIAL PRIMARY KEY,
     product_id INT REFERENCES products(product_id),
     change_quantity INT,
     change_type VARCHAR(50), -- e.g., 'restock', 'sale', etc.
     changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+-- DROP TABLE IF EXISTS customers, products, orders, order_items, inventory CASCADE;
