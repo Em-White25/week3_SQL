6d34d394f36b1b430deb5c41383e30ee541a8309
White <emmanuel.white@amalitechtraining.org>
2025-04-13 19:26:10 +0000

    tables creation script

---
+++ schema_design/schema.sql
@@ -0,0 +1,48 @@
+-- Customers table
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    customer_name VARCHAR(100),
+    email VARCHAR(100),
+    phone_number VARCHAR(20),
+    country VARCHAR(50),
+    city VARCHAR(50)
+);
+
+-- Products table
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price NUMERIC(10, 2),
+    stock_quantity INT,
+    reorder_level INT,
+    description TEXT
+);
+
+-- Orders table
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    order_date DATE,
+    total_amount NUMERIC(10, 2),
+    status VARCHAR(20)
+);
+
+-- Order Items table
+CREATE TABLE order_items (
+    details_id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(order_id),
+    product_id INT REFERENCES products(product_id),
+    quantity INT,
+    unit_price NUMERIC(10, 2),
+    total_amount NUMERIC(10, 2)
+);
+
+-- Inventory table
+CREATE TABLE inventory (
+    log_id SERIAL PRIMARY KEY,
+    product_id INT REFERENCES products(product_id),
+    change_quantity INT,
+    change_type VARCHAR(50), -- e.g., 'restock', 'sale', etc.
+    changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
